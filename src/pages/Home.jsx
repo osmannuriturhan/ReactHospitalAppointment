45cc55e9aef6d4dd6cabb5c4562df69705459051
osmn_turhn <osmannuri.turhan01@gmail.com>
2023-12-11 23:02:07 +0300

    Add Local Storage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
 import AppointmentList from "../components/AppointmentList";
 import Doctors from "../components/Doctors";
-import { appointmentData } from "../helper/Data";
+// import { appointmentData } from "../helper/Data";
 
 const Home = () => {
-  const [appointment, setAppointment] = useState(appointmentData);
+  // const [appointment, setAppointment] = useState(appointmentData);
+  const [appointment, setAppointment] = useState(
+    JSON.parse(localStorage.getItem("list")) || []
+  );
 
   const handleAdd = (newAppointment) => {
     setAppointment([...appointment, newAppointment]);
+    localStorage.setItem(
+      "list",
+      JSON.stringify([...appointment, newAppointment])
+    );
   };
 
   const handleDelete = (id) => {
     const filteredAppointment = appointment.filter((item) => item.id !== id);
     setAppointment(filteredAppointment);
+    localStorage.setItem("list", JSON.stringify(filteredAppointment));
   };
 
   const handleDoubleClick = (id) => {
@@ -23,6 +31,7 @@ const Home = () => {
       return item;
     });
     setAppointment(updatedAppointment);
+    localStorage.setItem("list", JSON.stringify(updatedAppointment));
   };
 
   return (
